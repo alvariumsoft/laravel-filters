d3a31ec1cb7539faa51fc2e57ae558be7495af77
hammer <dimaskriplyonok@gmail.com>
2020-03-31 13:32:22 +0300

    readme complete

--- app/Alvarium/Filters/Services/Links/Defaults/DefaultLinksCreator.php
+++ app/Alvarium/Filters/Services/Links/Defaults/DefaultLinksCreator.php
@@ -27,7 +27,6 @@ class DefaultLinksCreator extends LinksCreator
     {
         $newChosenFilters = [];
 
-        // todo: refactor this function
         foreach ($chosenFilters as $key => $chosenFilter) {
             $newChosenFilters[$key] = $chosenFilter;
             foreach ($chosenFilter['items'] as $keyItem => $item) {
@@ -70,7 +69,6 @@ class DefaultLinksCreator extends LinksCreator
     {
         $newFilters = [];
 
-        // todo: refactor this function
         foreach ($filters as $key => $chosenFilter) {
             $newFilters[$key] = $chosenFilter;
             foreach ($chosenFilter['items'] as $keyItem => $item) {
